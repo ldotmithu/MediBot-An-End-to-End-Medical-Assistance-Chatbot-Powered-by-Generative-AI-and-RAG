b7a35966d78e124d96da77771c25201322689517
ldotmithu <ldotmithurshan222@gmail.com>
2025-02-28 19:40:29 +0530

    add

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version="0.0.0",
     author="ldotmithu",
     author_email="ldotmithurshan222@gmail.com",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires = []
 )
